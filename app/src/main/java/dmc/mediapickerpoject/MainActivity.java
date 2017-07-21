d33e6a40a8f216798c7797f3caaa6e5dc3293e40
dmcBig <yu911512152@gmail.com>
2017-07-21 15:51:07 +0800

    demo

--- app/src/main/java/dmc/mediapickerpoject/MainActivity.java
+++ app/src/main/java/dmc/mediapickerpoject/MainActivity.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
 
-    ArrayList<Media> select;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,16 +31,16 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    ArrayList<Media> select;
     void go(){
         Intent intent =new Intent(MainActivity.this, PickerActivity.class);
-        intent.putExtra(PickerConfig.SELECT_MODE,PickerConfig.PICKER_IMAGE_VIDEO);
+        intent.putExtra(PickerConfig.SELECT_MODE,PickerConfig.PICKER_IMAGE_VIDEO);//default image and video (Optional)
         long maxSize=188743680L;//long long long
-        intent.putExtra(PickerConfig.MAX_SELECT_SIZE,maxSize);
-        intent.putExtra(PickerConfig.MAX_SELECT_COUNT,15);
-        intent.putExtra(PickerConfig.DEFAULT_SELECTED_LIST,select);
+        intent.putExtra(PickerConfig.MAX_SELECT_SIZE,maxSize); //default 180MB (Optional)
+        intent.putExtra(PickerConfig.MAX_SELECT_COUNT,15);  //default 40 (Optional)
+        intent.putExtra(PickerConfig.DEFAULT_SELECTED_LIST,select); // (Optional)
         MainActivity.this.startActivityForResult(intent,200);
     }
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
