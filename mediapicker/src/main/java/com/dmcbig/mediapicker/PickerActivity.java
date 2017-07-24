b1992773ebb4b64114d662b393ba442a12fa480f
dmcBig <yu911512152@gmail.com>
2017-07-24 16:49:16 +0800

    Permission

--- mediapicker/src/main/java/com/dmcbig/mediapicker/PickerActivity.java
+++ mediapicker/src/main/java/com/dmcbig/mediapicker/PickerActivity.java
@@ -1,12 +1,15 @@
 package com.dmcbig.mediapicker;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.ListPopupWindow;
 import android.support.v7.widget.RecyclerView;
@@ -28,11 +31,14 @@ import com.dmcbig.mediapicker.utils.ScreenUtils;
 
 import java.util.ArrayList;
 
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
 /**
  * Created by dmcBig on 2017/6/9.
  */
 
-public class PickerActivity extends Activity implements DataCallback ,View.OnClickListener{
+public class PickerActivity extends AppCompatActivity implements DataCallback ,View.OnClickListener{
 
     Intent argsIntent;
     RecyclerView recyclerView;
@@ -47,7 +53,7 @@ public class PickerActivity extends Activity implements DataCallback ,View.OnCli
         setContentView(R.layout.main);
         recyclerView=(RecyclerView) findViewById(R.id.recycler_view);
         findViewById(R.id.btn_back).setOnClickListener(this);
-        ((TextView)findViewById(R.id.bar_title)).setText(argsIntent.getIntExtra(PickerConfig.SELECT_MODE,101)==101?getResources().getString(R.string.select_title):getResources().getString(R.string.select_image_title));
+        ((TextView)findViewById(R.id.bar_title)).setText(argsIntent.getIntExtra(PickerConfig.SELECT_MODE,101)==101?getString(R.string.select_title):getString(R.string.select_image_title));
         done=(Button) findViewById(R.id.done);
         category_btn=(Button) findViewById(R.id.category_btn);
         preview=(Button) findViewById(R.id.preview);
@@ -95,11 +101,16 @@ public class PickerActivity extends Activity implements DataCallback ,View.OnCli
         });
     }
 
+    @AfterPermissionGranted(119)
     void getMediaData(int type){
-        if(type==PickerConfig.PICKER_IMAGE_VIDEO){
-            getLoaderManager().initLoader(type,null,new MediaLoader(this,this));
-        }else{
-            getLoaderManager().initLoader(type,null,new ImageLoader(this,this));
+        if (EasyPermissions.hasPermissions(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            if(type==PickerConfig.PICKER_IMAGE_VIDEO){
+                getLoaderManager().initLoader(type,null,new MediaLoader(this,this));
+            }else{
+                getLoaderManager().initLoader(type,null,new ImageLoader(this,this));
+            }
+        } else {
+            EasyPermissions.requestPermissions(this, getString(R.string.READ_EXTERNAL_STORAGE),119, Manifest.permission.READ_EXTERNAL_STORAGE);
         }
     }
 
@@ -152,4 +163,9 @@ public class PickerActivity extends Activity implements DataCallback ,View.OnCli
         super.onConfigurationChanged(newConfig);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
 }
