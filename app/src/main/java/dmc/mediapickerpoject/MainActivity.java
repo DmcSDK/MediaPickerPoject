859edc4e5b42948d098a4ef05e55dfdf5e1ab23f
dmcBig <yu911512152@gmail.com>
2017-11-14 15:59:04 +0800

    add takePhoto with camera

--- app/src/main/java/dmc/mediapickerpoject/MainActivity.java
+++ app/src/main/java/dmc/mediapickerpoject/MainActivity.java
@@ -1,17 +1,14 @@
 package dmc.mediapickerpoject;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-
 
 import com.dmcbig.mediapicker.PickerActivity;
 import com.dmcbig.mediapicker.PickerConfig;
 import com.dmcbig.mediapicker.entity.Media;
-
-
 
 import java.util.ArrayList;
 
@@ -33,12 +30,15 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Media> select;
     void go(){
         Intent intent =new Intent(MainActivity.this, PickerActivity.class);
-        intent.putExtra(PickerConfig.SELECT_MODE,PickerConfig.PICKER_VIDEO);//default image and video (Optional)
+        intent.putExtra(PickerConfig.SELECT_MODE,PickerConfig.PICKER_IMAGE_VIDEO);//default image and video (Optional)
         long maxSize=188743680L;//long long long
         intent.putExtra(PickerConfig.MAX_SELECT_SIZE,maxSize); //default 180MB (Optional)
         intent.putExtra(PickerConfig.MAX_SELECT_COUNT,15);  //default 40 (Optional)
         intent.putExtra(PickerConfig.DEFAULT_SELECTED_LIST,select); // (Optional)
         MainActivity.this.startActivityForResult(intent,200);
+
+//        Intent intent =new Intent(MainActivity.this, TakePhotoActivity.class); //Take a photo with a camera
+//        MainActivity.this.startActivityForResult(intent,200);
     }
 
     @Override
---
+++ mediapicker/src/main/java/com/dmcbig/mediapicker/TakePhotoActivity.java
@@ -0,0 +1,85 @@
+package com.dmcbig.mediapicker;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
+import android.widget.Toast;
+
+import com.dmcbig.mediapicker.entity.Media;
+import com.dmcbig.mediapicker.utils.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by dmcBig on 2017/11/14.
+ */
+
+public class TakePhotoActivity extends Activity{
+    Uri NuriForFile ;
+    File mTmpFile = null;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+
+        try {
+            mTmpFile =  createImageFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {//如果大于等于7.0使用FileProvider
+            NuriForFile=FileProvider.getUriForFile(this,this.getPackageName() + ".share", mTmpFile);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, NuriForFile);
+            startActivityForResult(intent, 100);
+        } else {
+            if(mTmpFile != null && mTmpFile.exists()) {
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mTmpFile));
+                startActivityForResult(intent, 101);
+            }else{
+                Toast.makeText(this, "take error", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ArrayList<Media> medias=new ArrayList<>();
+        if(requestCode==100||requestCode==101&&resultCode==RESULT_OK){
+            Media media=new Media(mTmpFile.getPath(),mTmpFile.getName(),0,1,0,0,"");
+            medias.add(media);
+            Intent intent=new Intent();
+            intent.putParcelableArrayListExtra(PickerConfig.EXTRA_RESULT,medias);
+            setResult(PickerConfig.RESULT_CODE,intent);
+            finish();
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        return image;
+    }
+}
